538eac6b983c769527f82fbfe5984f648cb21e85
PhilusOfficial <luciricofedefora@gmail.com>
2019-10-16 09:56:13 +0200

    finished

--- sketch.js
+++ sketch.js
@@ -1,18 +1,22 @@
 var mySong;
-var myImage;
+var rainbowblur;
+var rainbow;
 var marioImage;
+var fft;
 
 function preload(){
   mySong = loadSound("./assets/rainbowroad.mp3");
-  myImage = loadImage("./assets/rainbow.jpg");
+  rainbowblur = loadImage("./assets/rainbow.jpg");
+  rainbow = loadImage("./assets/rainbow2.jpg");
   marioImage = loadImage("./assets/mario.png");
 }
 
 function setup() {
   createCanvas(windowWidth,windowHeight)
-
+  angleMode(DEGREES);
   analyzer = new p5.Amplitude();
   analyzer.setInput(mySong);
+  fft = new p5.FFT();
 }
 
 function mousePressed() {
@@ -25,38 +29,51 @@ function mousePressed() {
 
 function draw() {
   var volume = 0;
+  var spectrum = fft.analyze();
 
-  if ( mySong.isPlaying() ) {
+  if ( mySong.isPlaying()) {
     imageMode(CENTER);
-    image(myImage, width/2, height/2, width, height);
+    image(rainbow, width/2, height/2, width, 1080);
   } else {
     push();
     imageMode(CENTER);
-    image(myImage, width/2, height/2, width, height);
-    noStroke();
-    fill('rgba(0,0,0, 0.5)');
-    rect(0,0,width, height);
+    image(rainbowblur, width/2, height/2, width, 1080);
     pop();
   }
 
-  for (var i = 0; i<360; i++){
-         var angle = i;
-         volume = analyzer.getLevel();
-         volume = map(volume, 0, 1, 0, height)
-         var r = 100 + volume/4*3 + volume/4*random();
-         var ray = 100;
-         var x = r*cos(angle);
-         var y = r*sin(angle);
-         var borderx = ray*cos(angle);
-         var bordery = ray*sin(angle);
+  for (var i = 0; i< spectrum.length-200; i+=10){
+    //remap the lenght of the lines of the pattern
+    var h = map(spectrum[i], 30, 255, 0, 70);
+    if(h<0){
+     //set the values under zero back to zero
+     h=0;
+   }
 
-         strokeWeight(2);
-         stroke('white');
-         line(borderx+width/2,bordery+height/2,x+width/2,y+height/2);
-       }
 
+    //create half of the audio pattern
+    var x = map(i, 0, spectrum.length-200, 180, 0);
+      stroke('white');
+      strokeWeight(2);
+      push();
+      translate(width/2,height/2);
+      rotate(x+45);
+      //draw half of the circle
+      line(cos(x)+100, sin(x)+100, cos(x)+100+h, sin(x)+100+h);
+    pop();
+
+    var x = map(i, spectrum.length-200, 0, 360, 180);
+    push();
+      translate(width/2,height/2);
+      rotate(x+45);
+      //draw the other half
+
+      line(cos(x)+100, sin(x)+100, cos(x)+100+h, sin(x)+100+h);
+    pop();
+
+  }
+//create a mario logo that changes size depending on the volume
   volume = analyzer.getLevel();
   volume = map(volume, 0, 1, 0, height)
   imageMode(CENTER);
-  image(marioImage, width/2, height/2, 80+volume/2, 80+volume/2);
+  image(marioImage, width/2, height/2, 120+volume/2, 120+volume/2);
 }
